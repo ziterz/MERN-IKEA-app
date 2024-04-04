fcd6706eff5cc7546c119fa244774b05c187f81b
Ziterz <ziady.mubaraq@gmail.com>
2024-04-04 21:31:42 +0700

    feat: change mongoose db connection config

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,14 +4,12 @@ import app from './app';
 
 const port = process.env.PORT || 3000;
 
-(async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
-    console.log('Connected to MongoDB Successfully');
-    app.listen(port, () => {
-      console.log('Listening on port ' + process.env.PORT);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-})();
+const main = async () => {
+  await mongoose.connect(process.env.MONGODB_URI as string);
+  console.log('Connected to MongoDB Successfully');
+  app.listen(port, () => {
+    console.log('Listening on port ' + process.env.PORT);
+  });
+};
+
+main().catch((err) => console.log(err));
